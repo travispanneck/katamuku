19553449f8c2c793abd42282540824ed3faf5dd3
nhaglind <nhaglind@gmail.com>
2016-07-12 11:51:44 -0500

    change background color with javascript

--- source/javascripts/countingcards.js
+++ source/javascripts/countingcards.js
@@ -3,10 +3,13 @@ var count = 0;
 function betOrHold () {
   if (count > 0) {
     document.getElementById('decision').textContent= "Bet";
+    document.getElementById('decision').style.backgroundColor = "rgb(135, 195, 135)";
   } else if (count < 0) {
     document.getElementById('decision').textContent= "Hold";
+    document.getElementById('decision').style.backgroundColor = "rgb(224, 93, 93)";
   } else {
     document.getElementById('decision').textContent= "No guidance";
+    document.getElementById('decision').style.backgroundColor = "rgb(195, 194, 135)";
   }
 }
 
