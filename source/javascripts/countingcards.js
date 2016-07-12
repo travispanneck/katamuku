1a1715c2db0dbe4c799e675cc008a85698e305b0
nhaglind <nhaglind@gmail.com>
2016-07-12 09:47:46 -0500

    update to fonts and button spacing

--- source/javascripts/countingcards.js
+++ source/javascripts/countingcards.js
@@ -1,7 +1,27 @@
-var count = 0;
+var cardCount = 0;
 
-function cardCounting () {
-  console.log("Hah, let's cheat!");
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      cardCount += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      cardCount += 0;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      cardCount -= 1;
+  }
+  return cardCount + (cardCount > 0 ? " Bet" : " Hold");
 }
 
-cardCounting();
+cc(2); cc(3); cc(7); cc('K'); cc('A');
